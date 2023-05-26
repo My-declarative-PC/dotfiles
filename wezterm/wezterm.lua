38d393b48d4861e67b4f63004a0ff93804d298bd
Timofey Sitnikov <tima.sitnikov@mail.ru>
2023-05-26 22:59:41 +0300

    Change font on 'JetBrainsNerdFont'

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
     color_scheme = "Dracula (Official)",
-    font = wezterm.font("JetBrains Mono"),
+    font = wezterm.font("JetBrainsMonoNerdFontMono Mono"),
     tab_bar_at_bottom = true,
     use_fancy_tab_bar = false,
     window_decorations = "RESIZE",
