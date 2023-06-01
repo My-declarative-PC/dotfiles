70f4f7608b2991519dcd975fe0ecab3f78ed1d8b
ImperatorMarsa <tima.sitnikov@mail.ru>
2023-06-01 14:28:30 +0300

    Добавил псивдонимы
    для отображения списка файлов

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,34 @@ function fish_greeting
     end
 end
 
-# vatibles
+### Aliases
+function ls
+    if not command -sq lsd
+        ls $argv
+    end
+
+    lsd $argv
+end
+
+function la
+    if not command -sq lsd
+        ls -hal $argv
+    end
+
+    lsd -la $argv
+end
+
+function ll
+    if not command -sq lsd
+        ls -hl $argv
+    end
+
+    lsd -l $argv
+end
+
+### Vatibles
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
+### Initializations
 starship init fish | source
 zoxide init fish | source
