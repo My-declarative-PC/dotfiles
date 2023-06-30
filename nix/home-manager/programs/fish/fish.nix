4387154db0ddbf74c1587f5c4e7c0e9adc4c5d0e
Timofey Sitnikov <tima.sitnikov@mail.ru>
2023-06-30 23:17:41 +0300

    add color theme for `fzf`

--- nix/home-manager/programs/fish/fish.nix
+++ nix/home-manager/programs/fish/fish.nix
@@ -5,6 +5,7 @@
     enable = true;
     shellAliases = { mkdir = "mkdir -p"; };
     interactiveShellInit = ''
+      set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
       ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
     '';
     functions = {
