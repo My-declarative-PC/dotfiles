4326086facf7369e050c2e9d612be45c7d3523f7
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-05-28 23:22:46 +0300

    доперенёс конфиги профиля

---
+++ dot_profile.d/custom_init.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if command -v fastfetch &>/dev/null; then
+    clear
+    fastfetch
+fi
+
+#
+# Aliases
+#
+
+function lsusb {
+    if command -v cyme &>/dev/null; then
+        cyme "$@"
+    else
+        lsusb "$@"
+    fi
+}
+
+function cat {
+    if command -v bat &>/dev/null; then
+        bat "$@"
+    else
+        cat "$@"
+    fi
+}
+
+function rg {
+    if command -v batgrep &>/dev/null; then
+        batgrep "$@"
+    else
+        rg "$@"
+    fi
+}
+
+### ls
+function ls {
+    if command -v eza &>/dev/null; then
+        eza -1 --icons "$@"
+    else
+        ls "$@"
+    fi
+}
+
+function ll {
+    ls -lh "$@"
+}
+
+function la {
+    ll -a "$@"
+}
+
+function tree {
+    if command -v eza &>/dev/null; then
+        ls -T "$@"
+    else
+        tree "$@"
+    fi
+}
+
+#
+# Initializations
+#
+
+if command -v starship &>/dev/null; then
+    eval "$(starship init bash)"
+fi
+
+if command -v zoxide &>/dev/null; then
+    eval "$(zoxide init --cmd cd bash)"
+fi
+
+if command -v fzf &>/dev/null; then
+    eval "$(fzf --bash)"
+fi
+
+if command -v direnv &>/dev/null; then
+    eval "$(direnv hook bash)"
+fi
+
+if command -v atuin &>/dev/null; then
+    eval "$(atuin init bash)"
+fi
---
+++ dot_profile.d/custom_profile.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# PATH
+export PATH="$PATH:/var/lib/flatpak/exports/bin"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.local/share/flatpak/exports/share"
+# PATH end
+
+# homebrew
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
+export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
+# homebrew end
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+# GTK
+export GTK_USE_PORTAL=1
+export FLAVOR="mocha"
+export ACCENT="mauve"
+export GTK_THEME=catppuccin-$FLAVOR-$ACCENT-standard+default
+export ADW_DISABLE_PORTAL=1
+export GTK2_RC_FILES=/usr/share/themes/$GTK_THEME
+export XCURSOR_THEME=catppuccin-$FLAVOR-$ACCENT-cursors
+# GTK end
+
+# fzf
+source ~/.profile.d/fzf-theme.sh
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS: \
+--preview \"bat --color=always --style=numbers,changes,plain --line-range=:500 {}\"
+"
+# fzf end
+
+export EDITOR=hx
+export QT_STYLE_OVERRIDE=kvantum
+export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
+export XAUTHORITY=$HOME/.Xauthority
