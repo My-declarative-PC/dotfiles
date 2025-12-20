77d19a542dc45036a67176fdfe9cba9c8b65534f
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-12-20 15:14:59 +0300

    Покрасил раскаладки окон в палитру Catppuccin

--- dot_config/qtile/modules/layouts.py
+++ dot_config/qtile/modules/layouts.py
@@ -1,21 +1,30 @@
+from catppuccin import PALETTE
 from libqtile import layout
 from libqtile.config import Match
-from .utils import colors
+
+from .utils import general
+
+theme_name = general.get("theme", "mocha").lower()
+flavor = getattr(PALETTE, theme_name, PALETTE.mocha)
+font = general.get("font", "FiraCode Nerd Font")
+fontsize = general.get("fontsize", 12)
 
 # Используем цвета из TOML
 layouts = [
     layout.Columns(
         margin=6,
-        border_focus_stack=[
-            colors.get("focus", "#d75f5f"),
-            colors.get("unfocus", "#8f3d3d"),
-        ],
-        border_width=4,
+        border_focus=flavor.colors.lavender.hex,
+        border_normal=flavor.colors.surface0.hex,
+        border_focus_stack=[flavor.colors.mauve.hex, flavor.colors.blue.hex],
+        border_width=2,
     ),
     layout.Max(margin=3),
 ]
 
 floating_layout = layout.Floating(
+    border_focus=flavor.colors.lavender.hex,
+    border_normal=flavor.colors.surface0.hex,
+    border_width=4,
     float_rules=[
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),
@@ -24,5 +33,5 @@ floating_layout = layout.Floating(
         Match(wm_class="ssh-askpass"),
         Match(title="branchdialog"),
         Match(title="pinentry"),
-    ]
+    ],
 )
