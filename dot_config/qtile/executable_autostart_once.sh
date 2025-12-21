eaef96c2f64e15f69af06cd3a1285957ee71248f
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-12-21 16:25:02 +0300

    Расширил программы для автозапуска

--- dot_config/qtile/executable_autostart_once.sh
+++ dot_config/qtile/executable_autostart_once.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+dbus-launch /usr/bin/dunst &
+greenclip daemon >/dev/null &
+kdeconnect-indicator &
+mkdir -p /home/$USER/Pictures/Screenshots/ &
+nm-applet &
 picom -b &
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 sleep 2 && setxkbmap -layout us,ru -variant qwerty &
+/usr/bin/dunst &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+yandex-disk-indicator &
